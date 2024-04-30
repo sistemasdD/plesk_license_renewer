6e6aacc57cb5c4e0fa60af026698b3f79f2dff92
Sistemas digitalDot <164342086+sistemasdD@users.noreply.github.com>
2024-04-30 14:18:50 +0200

    Add files via upload

--- renewer.sh
+++ renewer.sh
@@ -3,11 +3,16 @@
 RESET=$(tput sgr0)
 RED=$(tput setaf 1)
 L_BLUE=$(tput setaf 159)
+LL_BLUE=$(tput setaf 219)
 L_PURPLE=$(tput setaf 200)
 BOLD=$(tput bold)
 
 ctrl_c(){
 	echo -e "${RED}[!] SIGINT sent to ${0} Process.\nExiting...${NC}"; exit 0
+}
+
+removeTMPFiles(){
+	rm -rf "${mktempNot12021}" "${mktemp12021}"; exit 0
 }
 
 banner(){
@@ -51,6 +56,7 @@ HELP
 }
 
 trap ctrl_c INT
+trap removeTMPFiles EXIT
 
 checkFileType(){
 	local vps_regex="^\[VPS\]$"	
@@ -82,7 +88,7 @@ checkAliveHost(){
 			local status=1
 
 			timeout 1 bash -c "ping -c1 ${ip}" &>/dev/null && \
-			{ status=0 && echo -e "${L_BLUE}[+] $(dig -x ${ip} +short) Alive${RESET}" && aliveHosts+=(${ip}) ; }
+			{ status=0 && echo -e "${L_BLUE}[*] $(dig -x ${ip} +short) Alive${RESET}" && aliveHosts+=(${ip}) ; }
 
 			[[ "${status}" -eq 1 ]] && { \
 				for port in $(seq 1 100); do
@@ -92,35 +98,49 @@ checkAliveHost(){
 
 				} || \
 				
-			[[ "${status}" -eq 1 ]] && echo -e "${RED}[*] ${ip} Seems Inactive or ICMP Packet Rejected :(${RESET}"
+			[[ "${status}" -eq 1 ]] && echo -e "${RED}[!] ${ip} Seems Inactive or ICMP Packet Rejected :(${RESET}"
 		done
 	fi
 }
 
 checkSSHPort(){
+	local server
 	local ip
 	local -a servers12021
-	local -a not12021Servers	
-	while IFS= read -r server; do
-		timeout 1 bash -c "echo '' > /dev/tcp/${server}/12021" &>/dev/null && servers12021+=(${server}) || \
-		not12021Servers+=(${server})
-	done < <(getIPs)
+	local -a not12021Servers
+	declare mktemp12021=$(mktemp)
+	declare mktempNot12021=$(mktemp)
+	#[[ -n "${1}" ]] && declare mktemp12021=$(mktemp) && declare mktempNot12021=$(mktemp)
 
-	#{ [[ "${r_flag}" == true && "${c_flag}" == false ]] || [[ "${1}" == "IPS" ]] ; } && { \
+	while IFS= read -r server; do
+		#timeout 1 bash -c "echo '' > /dev/tcp/${server}/12021" &>/dev/null && servers12021+=(${server}) || \
+		#not12021Servers+=(${server})
+
+		(
+			timeout 1 bash -c "echo '' > /dev/tcp/${server}/12021" &>/dev/null
+
+			[[ ${?} -eq 0 ]] && echo "${server}" >> "${mktemp12021}" || echo "${server}" >> "${mktempNot12021}" 
+		) &
+
+	done < <(getIPs); wait
+
+	mapfile -t servers12021 < "${mktemp12021}"
+	mapfile -t not12021Servers < "${mktempNot12021}"
+
 	{ [[ "${r_flag}" == true && "${c_flag}" == false ]] || [[ "${1}" == "IPS" ]] ; } && { \
 
 		for ip in "${servers12021[@]}"; do
 			printf "%s\n" "${ip}"
-		done;exit 0
+		done
 
 	}
 	
-	{ [[ "${r_flag}" == false ]] && [[ "${c_flag}" == true ]] ; } && { \
+	{ [[ "${r_flag}" == false ]] && [[ "${c_flag}" == true ]] && [[ -z "${1}" ]]; } && { \
 
 		for ip in "${not12021Servers[@]}"; do
 			hostname=$(dig -x ${ip} +short)
-			[[ ! -z "${hostname}" ]] && echo -e "${RED}[*] 12021 Port closed on ${ip} -> $(dig -x ${ip} +short)${RESET}" || \
-			echo -e "${RED}[*] 12021 Port closed on ${ip} -> No Hostname Obtained neither :(${RESET}"
+			[[ ! -z "${hostname}" ]] && echo -e "${RED}[!] 12021 Port closed on ${ip} -> $(dig -x ${ip} +short)${RESET}" || \
+			echo -e "${RED}[!] 12021 Port closed on ${ip} -> No Hostname Obtained neither :(${RESET}"
 		done
 
 		[[ "${#not12021Servers[@]}" -eq 0 ]] && echo -e "${L_PURPLE}[+] 12021 Port Opened on all Hosts :)${RESET}\n"
@@ -130,41 +150,42 @@ checkSSHPort(){
 
 checkPleskLicense(){
 	local ip keynameOS keynameDS
-	local regex_ip="\d{1,3}(\.\d{1,3}){3}"
+	local regex_ip='^[0-9]{1,3}(\.[0-9]{1,3}){3}$'
 	local keyPath="/etc/sw/keys/keys"
 	local originServerIp=
-	local -a servers=($(checkSSHPort "IPS"))
 
-	{ [[ $(checkFileType "${mainfile}") == "VPS" ]] && originServerIP="IPVPS" ; } || \
-	{ [[ $(checkFileType "${mainfile}") == "DEDICATED" ]] && originServerIP="IPDEDICADO" ; }
+	#[[ "${c_flag}" == true ]] && local -a servers=($(checkSSHPort "IPS"))
 
-	keynameOS=$(ssh -p12021 root@${originServerIP} '[[ -n $(ls -A /etc/sw/keys/keys/key*) ]] && realpath /etc/sw/keys/keys/key*' | md5sum | awk '{print $1}')
+	{ [[ $(checkFileType "${mainfile}") == "VPS" ]] && originServerIP="151.80.59.30" ; } || \
+	{ [[ $(checkFileType "${mainfile}") == "DEDICATED" ]] && originServerIP="57.128.96.133" ; }
+
+	[[ -z "${1}" ]] && echo -e "${L_BLUE}[+] Extracting $(dig -x ${originServerIP} +short)'s MD5 Hash...${RESET}"
+	keynameOS=$(ssh -p12021 root@${originServerIP} '[[ -n $(ls -A /etc/sw/keys/keys/* 2>/dev/null) ]] && realpath /etc/sw/keys/keys/key* > /tmp/md5hash && md5sum /tmp/md5hash && rm -rf /tmp/md5hash' | awk '{print $1}') && \
+	[[ -z "${1}" ]] && echo -e "\n${L_BLUE}[+] MD5 Hash Obtained (${keynameOS})${RESET}\n"
 
 	{ [[ "${r_flag}" == false ]] && [[ "${c_flag}" == true ]] ; } && { \
-	
-		[[ -n "${keynameOS}" ]] && echo "${servers[@]}"; exit 1
+
+		local -a servers=($(checkSSHPort "IPS")) && [[ -n "${keynameOS}" ]] &&
 
 			for ip in "${servers[@]}"; do
-				[[ ${ip} =~ $regex_ip ]] && keynameDS=$(ssh -p12021 root@${ip} '[[ -n $(ls -A /etc/sw/keys/keys/key*) ]] && \
-				realpath /etc/sw/keys/keys/key*' | md5sum | awk '{print $1}')
+				(
+				[[ ${ip} =~ $regex_ip ]] && keynameDS=$(ssh -p12021 root@${ip} '[[ -n $(ls -A /etc/sw/keys/keys/* 2>/dev/null) ]] && realpath /etc/sw/keys/keys/key* > /tmp/md5hash && md5sum /tmp/md5hash && rm -rf /tmp/md5hash' | awk '{print $1}')
 
 				[[ "${keynameOS}" == "${keynameDS}" ]] && { \
 
-					echo -e "\n${L_PURPLE}[+] Plesk key's MD5 Hash on $(dig -x ${ip} +short) ("${keynameDS}") \
-					matches MD5 Hash on $(dig -x ${originServerIP} +short)'s Key ("${keynameDS}")${RESET}"
+					echo -e "${LL_BLUE}[*] Plesk key's MD5 Hash on $(dig -x ${ip} +short 2>/dev/null || ${ip}) ("${keynameDS}") matches :)${RESET}"
 					
-				} || echo -e "\n${RED}[!] Plesk Licenses' Hashes on $(dig -x ${ip} +short) and $(dig -x ${originServerIP} +short) \
-					does not match${RESET}"
+				} || echo -e "\n${RED}[!] Plesk Licenses' Hash on $(dig -x ${ip} +short) does not match :( (${keynameDS}"
+				) &
 
-			done || { echo -e "\n${RED} [!] Plesk License Directory ~ /etc/sw/keys/keys seems empty :(${RESET}" ; exit 1 ; }
+			done; wait || { echo -e "\n${RED} [!] Plesk License Directory ~ "/etc/sw/keys/keys" on $(dig -x ${ip} +short) seems empty :(${RESET}" ; exit 1 ; }
 	}
 
 	{ [[ "${r_flag}" == true ]] && [[ "${c_flag}" == false ]] ; } && { \
 	
 		[[ -n "${keynameOS}" ]] && { \
 
-			keynameDS=$(ssh -p12021 root@${1} '[[ -n $(ls -A /etc/sw/keys/keys/key*) ]] && \
-			realpath /etc/sw/keys/keys/key*' | md5sum | awk '{print $1}')
+			keynameDS=$(ssh -p12021 root@${1} '[[ -n $(ls -A /etc/sw/keys/keys/* 2>/dev/null) ]] && realpath /etc/sw/keys/keys/key* > /tmp/md5hash && md5sum /tmp/md5hash && rm -rf /tmp/md5hash' | awk '{print $1}')
 
 			[[ "${keynameOS}" == "${keynameDS}" ]] && return 0 || return 1
 
@@ -178,8 +199,8 @@ updatePleskLicense(){
 	local originServerIP=
 	local -a servers=($(checkSSHPort)) # or mapfile -t servers < <(checkSSHPort)
 
-	{ [[ $(checkFileType "${mainfile}") == "VPS" ]] && originServerIP="IPVPS" ;} || { \
-	[[ $(checkFileType "${mainfile}") == "DEDICATED" ]] && originServerIP="IPDEDICADO" ; }
+	{ [[ $(checkFileType "${mainfile}") == "VPS" ]] && originServerIP="151.80.59.30" ;} || { \
+	[[ $(checkFileType "${mainfile}") == "DEDICATED" ]] && originServerIP="57.128.96.133" ; }
 
 	checkFileType "${mainfile}" &>/dev/null || { echo -e "${RED}[!] ${mainfile} File is not [VPS] or [DEDICATED] valid format${RESET}\n"; exit 1; } 
 
@@ -195,20 +216,20 @@ updatePleskLicense(){
 		scp -3 scp://${originServerIP}:12021//etc/sw/keys/keys/* scp://${server}:12021//etc/sw/keys/keys &>/dev/null && \
 		ssh -p 12021 root@${server} 'service psa restart' &>/dev/null && { \
 		[[ "${?}" -eq 0 ]] && echo -e "${L_BLUE}[+] $(dig -x ${server} +short) Plesk's License seems Updated :/${RESET}" || \
-		echo -e "${RED}[*] $(dig -x ${server} +short) Plesk's License Upgrade Failed :( ${RESET}" ; }
+		echo -e "${RED}[*] $(dig -x ${server} +short) Plesk's License Upgrade Failed :( ${RESET}" ; } &
 
 	done; wait
 
-	for server in "${servers[@]}"; do
+	echo -e "\n${L_BLUE}[+] Checking Plesk License after Update...${RESET}\n"
 
-		echo -e "\n${L_BLUE}[+] Checking Plesk License after Update...${RESET}\n"
-		checkPleskLicense "${server}" && echo -e "${L_BLUE}[+] $(dig -x ${server} +short) Plesk's License correctly Updated :)${RESET}" || { \
+	for server in "${servers[@]}"; do
+		checkPleskLicense "${server}" && echo -e "${L_BLUE}[*] $(dig -x ${server} +short) Plesk's License correctly Updated :)${RESET}" || { \
 
 			ssh -p 12021 root@${server} 'rm -rf /etc/sw/keys/keys/*' &>/dev/null && \
 			scp -3 scp://${originServerIP}:12021//etc/sw/keys/keys/* scp://${server}:12021//etc/sw/keys/keys &>/dev/null && \
 			ssh -p 12021 root@${server} 'service psa restart' &>/dev/null && { \
-			[[ "${?}" -eq 0 ]] && echo -e "${L_BLUE}[+] $(dig -x ${server} +short) Plesk's License correcty Update :)${RESET}" || \
-			echo -e "${RED}[*] $(dig -x ${server} +short) Plesk's License Upgrade Failed :( ${RESET}" ; }
+			[[ "${?}" -eq 0 ]] && echo -e "${L_BLUE}[*] $(dig -x ${server} +short) Plesk's License correctly Update :)${RESET}" || \
+			echo -e "${RED}[!] $(dig -x ${server} +short) Plesk's License Upgrade Failed :( ${RESET}" ; }
 		}
 	done
 }
@@ -243,19 +264,19 @@ main(){
 	done
 
 	[[ "${#}" -le 2 ]] && banner && \
-	echo -e "\n\t${L_BLUE}[*] Not enough Parameters specified. Try ${0} -h to print Help${RESET}\n\n" && exit 0
+	echo -e "\n\t${L_BLUE}[!] Not enough Parameters specified. Try ${0} -h to print Help${RESET}\n\n" && exit 0
 
 	[[ -e "${mainfile}" ]] && { grep -qiPa "${file_regex}" "${mainfile}" || \
-	{ banner; echo -e "\n${RED}[+] File Format not supported or allowed${RESET}\n"; exit 1; } ; } || \
-	{ banner; echo -e "\n${RED}[+] ${mainfile} File not exists on system $(hostname --fqdn)${RESET}\n"; exit 1; }
+	{ banner; echo -e "\n${RED}[!] File Format not supported or allowed${RESET}\n"; exit 1; } ; } || \
+	{ banner; echo -e "\n${RED}[!] ${mainfile} File not exists on system $(hostname --fqdn)${RESET}\n"; exit 1; }
 
-	"${c_flag}" && echo -e "\n${L_BLUE}[+] Checking if Host is Alive...${RESET}\n" && echo -e "${L_PURPLE}$(checkAliveHost)${RESET}" && \
+	"${c_flag}" && echo -e "\n${L_BLUE}[+] Checking if Hosts are alive...${RESET}\n" && echo -e "${L_PURPLE}$(checkAliveHost)${RESET}" && \
 	echo -e "\n${L_BLUE}[+] Checking 12021 Port Connection on Servers...${RESET}\n" && echo -e "${L_PURPLE}$(checkSSHPort)${RESET}" && \
 	echo -e "\n${L_BLUE}[+] Checking Plesk License Status on Server...${RESET}\n" && echo -e "${L_PURPLE}$(checkPleskLicense)${RESET}\n"
 
-	"${l_flag}" && echo -e "\n${L_BLUE}[+] Extracting IP:Hostname list...\n${RESET}" && echo -e "\n${L_PURPLE}$(cat ${mainfile})\n${RESET}"
+	"${l_flag}" && echo -e "\n${L_BLUE}[+] Extracting IP:Hostname list...${RESET}" && echo -e "${L_PURPLE}$(grep -iPav '^\[.*\]$' ${mainfile})\n${RESET}"
 
-	"${r_flag}" && echo -e "\n${L_BLUE}[+] Updating Plesk Licenses...${RESET}\n" && updatePleskLicense
+	"${r_flag}" && echo -e "\n${L_BLUE}[+] Updating Plesk Licenses...${RESET}\n" && echo -e "\n${L_PURPLE}$(updatePleskLicense)${RESET}\n"
 }
 
 main "${@}"
